bd2190f5e0538222a20bed3ddcc4e3919915be63
jahrmarkt <jahn.marko@gmail.com>
2018-04-10 12:51:34 +0200

    test

---
+++ version3/main.py
@@ -0,0 +1,166 @@
+# Import ini
+from TowerGame.version3.model import *
+from TowerGame.version3.update import updateRun
+
+""""
+TODO 
+
+"""
+
+
+def main():
+    gameName = "Magic Towers"
+    screen.fill((255, 255, 255))
+    pygame.display.set_caption(gameName)
+
+
+    # Text
+    font = pygame.font.SysFont("impact", 24)
+    font2 = pygame.font.SysFont("impact", 36)
+    fontTitle = pygame.font.SysFont("impact", 48)
+    scoreColor = (255, 215, 0)  # gold
+    menuBgColor = (104, 34, 139)  # indigo
+
+    textHeader1 = fontTitle.render(gameName, True, scoreColor)
+    textHeader2 = font2.render("Pause", True, scoreColor)
+    textHeader3 = font2.render("You Lost", True, scoreColor)
+
+    textMenu1 = font2.render("Continue?", True, scoreColor)
+    textMenu2 = font2.render("Try Again?", True, scoreColor)
+    textMenu3 = font2.render("Press Enter", True, scoreColor)
+    textMenu4 = font2.render("Start Game?", True, scoreColor)
+
+    textScore = font.render(str(0), True, scoreColor)
+
+    screenHeaderY = height / 4
+    (screenMiddleX, screenMiddleY) = (width / 2, height / 2)
+
+    # Assign Key Variables
+    keepGoing = True
+    clock = pygame.time.Clock()
+    timer = 0
+
+    input = Input()
+    model = Model(width, height)
+    model.gameState = GameState.INTRO
+
+    # Loop
+    while keepGoing:
+        # Timer
+        clock.tick(50)
+        timer = timer + 1
+        input.reset()  # setze input zurück
+        input.timer = timer
+
+        # Input
+        for event in pygame.event.get():
+            if event.type == QUIT:
+                keepGoing = False
+                break
+            if event.type == KEYDOWN:
+                if event.key == K_RETURN:
+                    input.returnKey = True
+                elif event.key == K_0:  # numberkeys nur zum testen
+                    input.numberPressed = True
+                    input.numberKey = 0
+                elif event.key == K_1:
+                    input.numberPressed = True
+                    input.numberKey = 1
+                elif event.key == K_2:
+                    input.numberPressed = True
+                    input.numberKey = 2
+                elif event.key == K_3:
+                    input.numberPressed = True
+                    input.numberKey = 3
+                elif event.key == K_4:
+                    input.numberPressed = True
+                    input.numberKey = 4
+                elif event.key == K_5:
+                    input.numberPressed = True
+                    input.numberKey = 5
+                elif event.key == K_6:
+                    input.numberPressed = True
+                    input.numberKey = 6
+                elif event.key == K_7:
+                    input.numberPressed = True
+                    input.numberKey = 7
+                elif event.key == K_8:
+                    input.numberPressed = True
+                    input.numberKey = 8
+                elif event.key == K_9:
+                    input.numberPressed = True
+                    input.numberKey = 9
+            if event.type == MOUSEBUTTONDOWN:
+                input.mouseLeftPressed = event.button == 1
+                input.mouseRightPressed = event.button == 3
+                input.cursor = pygame.mouse.get_pos()
+            if event.type == MOUSEBUTTONUP:
+                input.mouseLeftReleased = event.button == 1
+                input.cursor = pygame.mouse.get_pos()
+            if event.type == MOUSEMOTION:
+                input.cursor = pygame.mouse.get_pos()
+
+        if model.gameState == GameState.RUN:
+            updateRun(input, screen, model)
+        elif model.gameState == GameState.INTRO:
+            # update
+            if input.returnKey:
+                model = Model(width, height)
+                model.gameState = GameState.RUN
+
+            # Redisplay
+            screen.fill(menuBgColor)
+
+            screen.blit(textHeader1,
+                        (screenMiddleX - textHeader1.get_width() // 2, screenHeaderY - textHeader1.get_height() // 2))
+
+            screen.blit(textMenu4,
+                        (screenMiddleX - textMenu4.get_width() // 2, screenMiddleY - textMenu4.get_height() // 2))
+            screen.blit(textMenu3,
+                        (screenMiddleX - textMenu3.get_width() // 2, screenMiddleY + 50 - textMenu3.get_height() // 2))
+            pygame.display.flip()
+
+        elif model.gameState == GameState.RESTART:
+            # update
+            if input.returnKey:
+                model = Model(width, height)
+                model.gameState = GameState.RUN
+
+            # Redisplay
+            screen.fill(menuBgColor)
+
+            screen.blit(textHeader3,
+                        (screenMiddleX - textHeader3.get_width() // 2, screenHeaderY - textHeader3.get_height() // 2))
+
+            textScore = font.render("Enemies Killed: " + str(model.enemiesKilled), True, scoreColor)
+            screen.blit(textScore,
+                        (screenMiddleX - textScore.get_width() // 2, screenMiddleY - textScore.get_height() // 2))
+            screen.blit(textMenu2,
+                        (screenMiddleX - textMenu2.get_width() // 2, screenMiddleY + 50 - textMenu2.get_height() // 2))
+            screen.blit(textMenu3,
+                        (screenMiddleX - textMenu3.get_width() // 2, screenMiddleY + 100 - textMenu3.get_height() // 2))
+            pygame.display.flip()
+
+        elif model.gameState == GameState.PAUSE:
+            # update
+            if input.returnKey:
+                model.gameState = GameState.RUN
+
+            # Redisplay
+            screen.fill(menuBgColor)
+
+            # Display text
+            screen.blit(textHeader2,
+                        (screenMiddleX - textHeader2.get_width() // 2, screenHeaderY - textHeader2.get_height() // 2))
+            textScore = font.render("Enemies Killed: " + str(model.enemiesKilled), True, scoreColor)
+            screen.blit(textScore,
+                        (screenMiddleX - textScore.get_width() // 2, screenMiddleY - textScore.get_height() // 2))
+            screen.blit(textMenu1,
+                        (screenMiddleX - textMenu1.get_width() // 2, screenMiddleY + 50 - textMenu1.get_height() // 2))
+            screen.blit(textMenu3,
+                        (screenMiddleX - textMenu3.get_width() // 2, screenMiddleY + 100 - textMenu3.get_height() // 2))
+
+        pygame.display.flip()
+
+
+main()
---
+++ version3/towerscript.py
@@ -0,0 +1,169 @@
+'''
+erzeugt den code für die tower
+damit ich nicht den code für die tower programmieren muss
+'''
+
+
+class DataBase(object):
+    def __init__(self):
+        self.output = ""
+
+        self.towerNames = []
+        self.ranges = []
+        self.shootTimes = []
+        self.isBoostings = []
+        self.projectileTypes = []
+        self.boostTypes = []
+
+        self.projectileNames = []
+        self.projSpeeds = []
+        self.projDamages = []
+
+        self.boostNames = []
+
+    def addAttackTower(self, name, range, shootTime, projType):
+        self.towerNames.append(name)
+        self.ranges.append(range)
+        self.shootTimes.append(shootTime)
+        self.isBoostings.append(False)
+        self.projectileTypes.append(projType)
+        self.boostTypes.append(None)
+
+    def addBoostTower(self, name, boostType):
+        self.towerNames.append(name)
+        self.ranges.append(10)
+        self.shootTimes.append(10)
+        self.isBoostings.append(True)
+        self.projectileTypes.append(None)
+        self.boostTypes.append(boostType)
+
+    def addProjectile(self, name, speed, damage):
+        self.projectileNames.append(name)
+        self.projSpeeds.append(speed)
+        self.projDamages.append(damage)
+
+    def generateOutput(self):
+        #0. header
+        self.output += \
+"""
+from enum import Enum
+from pygame.sprite import *
+
+
+pygame.init()
+pygame.mixer.init()
+size = (width, height) = 512, 704
+screen = pygame.display.set_mode(size)
+
+def loadTower(n):
+    #filename = "C:/Users/marko/Desktop/fh/OO Skriptsprachen/sprites/towers/" + n + ".png"
+    filename = "assets/sprites/towers/" + n + ".png"
+    
+    return pygame.image.load(filename).convert_alpha()
+
+def loadProjectile(n):
+    #filename = "C:/Users/marko/Desktop/fh/OO Skriptsprachen/sprites/projectiles/" + n + ".png"
+    filename = "assets/sprites/projectiles/" + n + ".png"
+    return pygame.image.load(filename).convert_alpha()
+    
+class BoostType(Enum):
+    SPEED = 1
+    DAMAGE = 2
+    RANGE = 3
+    DOUBLE = 4
+    
+"""
+
+
+        # 1. towerType
+        self.output += "class TowerType(Enum):" + "\n"
+        for index, name in enumerate(self.towerNames):
+            self.output += "\t" + name.upper() + " = " + str(index) + "\n"
+        self.output += "\n\n"
+
+        # 5.projectileType
+        self.output += "class ProjectileType(Enum):" + "\n"
+        for index, name in enumerate(self.projectileNames):
+            self.output += "\t" + name.upper() + " = " + str(index) + "\n"
+        self.output += "\n\n"
+
+        # 2. sprites Tower
+        for index, name in enumerate(self.towerNames):
+            self.output += "spriteTower" + name.title() + " = " + "loadTower('" + name + "')\n"
+        self.output += "\n\n"
+
+        # 3. icons Tower
+        for index, name in enumerate(self.towerNames):
+            self.output += "iconTower" + name.title() + " = " + "pygame.transform.scale(loadTower('" + name + "'), (64,64))\n"
+        self.output += "\n\n"
+
+        # 7. sprites Projectiles
+        for index, name in enumerate(self.projectileNames):
+            self.output += "spriteProjectile" + name.title() + " = " + "loadProjectile('" + name + "')\n"
+        self.output += "\n\n"
+
+        # 4.towerList
+        self.output += "towerList = \\\n{"
+        for i, name in enumerate(self.towerNames):
+            self.output += \
+                "\tTowerType." + name.upper() + " : (spriteTower" + name.title() + ", " + \
+                str(self.ranges[i]) + ", " + \
+                str(self.shootTimes[i]) + ", " + \
+                str(self.isBoostings[i]) + ", " + \
+                ("None" if self.projectileTypes[i] == None \
+                else "ProjectileType." + str(self.projectileTypes[i]).upper()) + ", " + \
+                ("None" if self.boostTypes[i] == None \
+                else "BoostType." + str(self.boostTypes[i]).upper()) + ", " + \
+                "iconTower" + name.title() + ")"
+            self.output += ", \n"
+        self.output += "}\n\n\n"
+
+        # 4.projectileList
+        self.output += "projectileList = \\\n{"
+        for i, name in enumerate(self.projectileNames):
+            self.output += \
+                "\tProjectileType." + name.upper() + " : (spriteProjectile" + name.title() + ", " + \
+                str(self.projSpeeds[i]) + ", " + \
+                str(self.projDamages[i]) + ", " + ")"
+            self.output += ", \n"
+
+        self.output += "}\n\n"
+
+    def saveFile(self):
+        filename = "towerData.py"
+        f = open(filename, "w")
+        f.write(self.output)
+
+
+data = DataBase()
+
+#(spr, range, shoottime)
+data.addAttackTower("fire", 100, 15, "fire")
+data.addAttackTower("rocky", 150, 30, "arrow")
+data.addAttackTower("yellow", 150, 30, "lightning1")
+data.addAttackTower("green", 150, 30, "poison")
+data.addAttackTower("redroof", 150, 30, "arrow")
+
+
+
+data.addBoostTower("high", "damage")
+data.addBoostTower("highblue", "range")
+data.addBoostTower("highbrown", "range")
+data.addBoostTower("rockyboost", "damage")
+data.addBoostTower("library", "double")
+
+
+#(spr, speed, damage)
+data.addProjectile("fire", 3, 4)
+data.addProjectile("lightning1", 2,1)
+data.addProjectile("lightning2", 2,1)
+data.addProjectile("arrow", 2,1)
+data.addProjectile("poison", 3,1)
+data.addProjectile("bomb", 1,1)
+
+
+
+data.generateOutput()
+data.saveFile()
+
+print(data.output)
